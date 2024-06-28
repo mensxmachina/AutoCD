e737a80630315b9d2166631c615d2e5e573e5a6a
Konstantina Biza <98031699+konstantb@users.noreply.github.com>
2024-06-28 16:32:45 +0300

    Add files via upload
    add knowledge about data type

--- predictive_modeling/class_predictive_configurator.py
+++ predictive_modeling/class_predictive_configurator.py
@@ -21,7 +21,7 @@ class PredictiveConfigurator():
         return (dict(zip(dicts, x)) for x in itertools.product(*dicts.values()))
 
 
-    def create_predictive_configs(self):
+    def create_predictive_configs(self, data_object):
 
         '''
         Creates list of predictive configurations
@@ -30,12 +30,16 @@ class PredictiveConfigurator():
         -------
             pred_configs (list of dictionaries) :  the predictive configurations
         '''
-        
+
+        data_type = data_object.data_type
         pred_configs = []
         for pred_name, pred_info in self.pred_algs.items():
-            for v in self._dict_product(pred_info):
-                for fs_name, fs_info in self.fs_algs.items():
-                    for k in self._dict_product(fs_info):
-                        pred_configs.append({"pred_name": pred_name} | v | {"fs_name": fs_name} | k)
+            if data_type in pred_info['data_type']:
+                # remove data type info from output combinations
+                del pred_info['data_type']
+                for v in self._dict_product(pred_info):
+                    for fs_name, fs_info in self.fs_algs.items():
+                        for k in self._dict_product(fs_info):
+                            pred_configs.append({"pred_name": pred_name} | v | {"fs_name": fs_name} | k)
 
         return pred_configs
